5571a1c6bada8d6f4b473d46439c7ba7864dded9
Amruthan2000 <45876883+Amruthan2000@users.noreply.github.com>
2020-05-18 20:52:51 +0530

    Update pwabuilder-sw.js

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,85 +1,54 @@
-
-  // This is the service worker with the Cache-first network
-
-const CACHE = "pwabuilder-precache";
-const precacheFiles = [
-  /* Add an array of files to precache for your app */
-];
-
-self.addEventListener("install", function (event) {
-  console.log("[PWA Builder] Install Event processing");
-
-  console.log("[PWA Builder] Skip waiting on install");
-  self.skipWaiting();
-
-  event.waitUntil(
-    caches.open(CACHE).then(function (cache) {
-      console.log("[PWA Builder] Caching pages during install");
-      return cache.addAll(precacheFiles);
-    })
-  );
-});
-
-// Allow sw to control of current page
-self.addEventListener("activate", function (event) {
-  console.log("[PWA Builder] Claiming clients for current page");
-  event.waitUntil(self.clients.claim());
-});
-
-// If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener("fetch", function (event) { 
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    fromCache(event.request).then(
-      function (response) {
-        // The response was found in the cache so we responde with it and update the entry
-
-        // This is where we call the server to get the newest version of the
-        // file to use the next time we show view
-        event.waitUntil(
-          fetch(event.request).then(function (response) {
-            return updateCache(event.request, response);
-          })
-        );
-
-        return response;
-      },
-      function () {
-        // The response was not found in the cache so we look for it on the server
-        return fetch(event.request)
-          .then(function (response) {
-            // If request was success, add or update it in the cache
-            event.waitUntil(updateCache(event.request, response.clone()));
-
-            return response;
-          })
-          .catch(function (error) {
-            console.log("[PWA Builder] Network request failed and no cache." + error);
-          });
-      }
-    )
-  );
-});
-
-function fromCache(request) {
-  // Check to see if you have it in the cache
-  // Return response
-  // If not in the cache, then return
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      if (!matching || matching.status === 404) {
-        return Promise.reject("no-match");
-      }
-
-      return matching;
-    });
-  });
-}
-
-function updateCache(request, response) {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.put(request, response);
-  });
-}
-
+// PWA Fire Bundle
+                      
+        // after a service worker is installed and the user navigates to a different page or 
+        // refreshes,the service worker will begin to receive fetch events
+                          
+        self.addEventListener('fetch', function(event) {
+        event.respondWith(caches.open('cache').then(function(cache) {
+        return cache.match(event.request).then(function(response) {
+        console.log("cache request: " + event.request.url);
+        var fetchPromise = fetch(event.request).then(function(networkResponse) {           
+        // if we got a response from the cache, update the cache                   
+        console.log("fetch completed: " + event.request.url, networkResponse);
+        if (networkResponse) {
+            console.debug("updated cached page: " + event.request.url, networkResponse);
+              cache.put(event.request, networkResponse.clone());}
+              return networkResponse;
+                  }, function (event) {   
+        // rejected promise - just ignore it, we're offline!   
+                  console.log("Error in fetch()", event);
+                  event.waitUntil(
+                  caches.open('cache').then(function(cache) { 
+        // our cache is named *cache* in the caches.open() above
+                  return cache.addAll
+                  ([            
+        //cache.addAll(), takes a list of URLs, then fetches them from the server
+        // and adds the response to the cache.           
+        // add your entire site to the cache- as in the code below; for offline access
+        // If you have some build process for your site, perhaps that could 
+        // generate the list of possible URLs that a user might load.               
+                '/', // do not remove this
+                '/index.html', //default
+                '/index.html?homescreen=1', //default
+                '/?homescreen=1', //default
+                '/assets/css/main.css',// configure as by your site ; just an example
+                '/images/*',// choose images to keep offline; just an example
+        // Do not replace/delete/edit the manifest.js paths below
+        //These are links to the extenal social media buttons that should be cached;
+        // we have used twitter's as an example
+              'https://platform.twitter.com/widgets.js',       
+                ]);
+                })
+                );
+                });
+        // respond from the cache, or the network
+          return response || fetchPromise;
+        });
+        }));
+        });
+        
+        self.addEventListener('install', function(event) {
+          // The promise that skipWaiting() returns can be safely ignored.
+          self.skipWaiting();
+          console.log("Latest version installed!");
+        });
